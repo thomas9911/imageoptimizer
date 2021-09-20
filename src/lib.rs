ef3c5da695a7068c00abd36103a2f7e436901a31
Thomas <thomastimmer11@hotmail.com>
2021-09-20 20:59:26 +0200

    feat: add extra function to specify the image formats

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,29 @@ pub mod formats;
 pub use error::Error;
 pub use formats::Format;
 
-/// Main function that converts the input image to the output image
+/// Main function that converts the input image to the output image. Detects the image format based on the file extension
 pub fn convert(input: &str, output: &str) -> Result<(), Box<dyn std::error::Error>> {
+    convert_explicit(
+        (input, Format::from_path(input)?),
+        (output, Format::from_path(output)?),
+    )
+}
+
+/// Main function that converts the input image to the output image. The format of each image need to be specified.
+pub fn convert_explicit(
+    input: (&str, Format),
+    output: (&str, Format),
+) -> Result<(), Box<dyn std::error::Error>> {
     use Format::*;
 
-    match (Format::from_path(input)?, Format::from_path(output)?) {
-        (Png, Png) => formats::png::convert(input, output)?,
-        (Jpeg, Jpeg) => formats::jpeg::convert(input, output)?,
-        (Jpeg, Png) => formats::png::convert_from_jpeg(input, output)?,
-        (Png, Jpeg) => formats::jpeg::convert_from_png(input, output)?,
-        (Png, Webp) => formats::webp::convert_from_png(input, output)?,
-        (Jpeg, Webp) => formats::webp::convert_from_jpeg(input, output)?,
-        (Svg, Svg) => formats::svg::convert(input, output)?,
+    match (input.1, output.1) {
+        (Png, Png) => formats::png::convert(input.0, output.0)?,
+        (Jpeg, Jpeg) => formats::jpeg::convert(input.0, output.0)?,
+        (Jpeg, Png) => formats::png::convert_from_jpeg(input.0, output.0)?,
+        (Png, Jpeg) => formats::jpeg::convert_from_png(input.0, output.0)?,
+        (Png, Webp) => formats::webp::convert_from_png(input.0, output.0)?,
+        (Jpeg, Webp) => formats::webp::convert_from_jpeg(input.0, output.0)?,
+        (Svg, Svg) => formats::svg::convert(input.0, output.0)?,
         _ => return Err(Error::boxed("unsupported input output convertion".into())),
     }
     Ok(())
